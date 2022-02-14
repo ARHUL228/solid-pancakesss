e6873aae3ff32bef6f2da30e465e932a2256de36
ARHUL228 <zelinskiy.kostya.kukurudza@gmail.com>
2022-02-14 20:00:11 +0200

    upload video

--- script.js
+++ script.js
@@ -79,3 +79,9 @@ muteButton.addEventListener('click',videoMute);
 volumeScale.addEventListener('change',videoChangeVolume);
 //Работа со скоростью
 speedSelect.addEventListener('change',videoChangeSpeed);
+document.getElementById("videoUpload")
+.onchange = function(event) {
+  let file = event.target.files[0];
+  let blobURL = URL.createObjectURL(file);
+  document.querySelector("video").src = blobURL;
+}
